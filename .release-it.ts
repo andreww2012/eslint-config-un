b35b15706fce2675d8d2e906af39f5e2d3010d74
Andrew Kazakov <tosk-nn@yandex.ru>
2024-07-17 11:21:28 +0300

    build: set `requireCleanWorkingDir: false` for `release-it`, downgrade `tsup` to 8.1.0

---
+++ .release-it.ts
@@ -0,0 +1,8 @@
+import type {Config} from 'release-it';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  git: {
+    requireCleanWorkingDir: false,
+  },
+} satisfies Config;
