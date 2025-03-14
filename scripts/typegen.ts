b300e2322d666539314945005514098c1961329e
Andrew Kazakov <tosk-nn@yandex.ru>
2025-03-14 15:12:31 +0300

    chore(typegen): do not set unnecessary configs, remove unused eslint-disable

--- scripts/typegen.ts
+++ scripts/typegen.ts
@@ -1,7 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
-// eslint-disable-next-line import/no-deprecated
 import {builtinRules} from 'eslint/use-at-your-own-risk';
 import {flatConfigsToRulesDTS} from 'eslint-typegen/core';
 import {eslintConfig} from '../src';
@@ -10,48 +9,15 @@ const configs = [
   {
     plugins: {
       '': {
-        // eslint-disable-next-line import/no-deprecated, @typescript-eslint/no-deprecated
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         rules: Object.fromEntries(builtinRules.entries()),
       },
     },
   },
   ...eslintConfig({
     configs: {
+      // If Angular is not found installed, plugin is not generated
       angular: true,
-      cli: true,
-      cssInJs: true,
-      deMorgan: true,
-      eslintComments: true,
-      import: true,
-      jest: {
-        jestExtended: true,
-      },
-      js: true,
-      jsdoc: true,
-      json: true,
-      markdown: true,
-      node: true,
-      packageJson: true,
-      perfectionist: true,
-      preferArrowFunctions: true,
-      promise: true,
-      qwik: true,
-      regexp: true,
-      security: true,
-      sonar: true,
-      tailwind: true,
-      toml: true,
-      ts: {
-        noTypeAssertion: true,
-      },
-      unicorn: true,
-      vitest: true,
-      vue: {
-        a11y: true,
-        nuxtMajorVersion: 3,
-        pinia: true,
-      },
-      yaml: true,
     },
   }),
 ];
