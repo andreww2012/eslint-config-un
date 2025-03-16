1d052a56044cfef434ed4d49a6b8f27f74cd4aa2
Andrew Kazakov <tosk-nn@yandex.ru>
2025-03-16 16:20:30 +0300

    build: add spell checker

---
+++ cspell.config.js
@@ -0,0 +1,59 @@
+// @ts-check
+
+/**
+ * @type {import('cspell').CSpellSettings}
+ */
+export default {
+  useGitignore: true,
+  ignorePaths: ['**/pnpm-lock.yaml'],
+  words: [
+    // Product names
+    'qwik',
+    'sonarjs',
+    'klona',
+    'nuxt',
+    'vuejs',
+    'unplugin',
+    'jsdocs',
+    'libxmljs',
+    'kramed',
+    'signale',
+    'opensearchservice',
+    'commonmark',
+    'shiki',
+
+    // Technical words/terms
+    'arraify',
+    'renamer',
+    'nonconstructor',
+    'nonoctal',
+    'thenables',
+    'callees',
+    'pseudorandomness',
+    'httponly',
+    'lookarounds',
+    'chunkname',
+    'namepaths',
+    'lockfiles',
+    'worklet',
+    'nums',
+
+    // Misc
+    'multicol',
+    'bgsound',
+    'noembed',
+    'scopedslots',
+    'setstate',
+    'mtsx',
+    'ctsx',
+    'rrggbb',
+    'ngsrc',
+    'ngmodule',
+    'andreww',
+
+    // Actual English words
+    'uncategorized',
+    'mischeck',
+    'unthrown',
+  ],
+};
