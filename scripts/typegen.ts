92d6584c497f3a00fd48273372a0b0f481c22aa3
Andrew Kazakov <tosk-nn@yandex.ru>
2025-05-11 13:08:59 +0300

    feat(scripts): print the diff during typegen

--- scripts/typegen.ts
+++ scripts/typegen.ts
@@ -1,29 +1,87 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import path from 'node:path';
-import url from 'node:url';
+import chalk from 'chalk';
+import * as diff from 'diff';
 import {flatConfigsToRulesDTS} from 'eslint-typegen/core';
 import {eslintConfig} from '../src';
 import {eslintPluginVanillaRules} from '../src/eslint';
 
-const configs = [
-  {
-    plugins: {
-      '': eslintPluginVanillaRules,
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+const __dirname = import.meta.dirname;
+
+await fs.mkdir(resolveInOutDir(), {recursive: true});
+
+const ruleTypes = await generateRuleTypes();
+
+const mostRecentRuleTypesFileName = (await fs.readdir(resolveInOutDir())).sort().at(-1);
+let diffString = '';
+if (mostRecentRuleTypesFileName) {
+  const mostRecentRuleTypes = await fs
+    .readFile(resolveInOutDir(mostRecentRuleTypesFileName), 'utf8')
+    .catch((error: unknown) => {
+      if (error && typeof error === 'object' && 'code' in error && error.code === 'ENOENT') {
+        return null;
+      }
+      throw error;
+    });
+  if (mostRecentRuleTypes) {
+    diffString =
+      mostRecentRuleTypes === ruleTypes
+        ? chalk.gray('No changes between the current and the most recent rule types')
+        : getDiffAsPatch(mostRecentRuleTypes, ruleTypes).join('\n');
+  }
+}
+console.log(diffString || chalk.gray('No most recent rule types found'));
+
+await Promise.all([
+  fs.writeFile(path.join(__dirname, '../src/eslint-types.d.ts'), ruleTypes),
+  fs.writeFile(
+    resolveInOutDir(`eslint-types.${new Date().toISOString().replaceAll(':', '')}.d.ts`),
+    ruleTypes,
+  ),
+]);
+
+async function generateRuleTypes(): Promise<string> {
+  const configs = [
+    {
+      plugins: {
+        '': eslintPluginVanillaRules,
+      },
     },
-  },
-  ...(await eslintConfig({
-    loadPluginsOnDemand: false,
-    configs: {
-      // If Angular is not found installed, plugin is not generated
-      angular: true,
-    },
-  })),
-];
+    ...(await eslintConfig({
+      loadPluginsOnDemand: false,
+      configs: {
+        // If Angular is not found installed, plugin is not generated
+        angular: true,
+      },
+    })),
+  ];
 
-const output = await flatConfigsToRulesDTS(configs as Parameters<typeof flatConfigsToRulesDTS>[0], {
-  includeAugmentation: false,
-});
+  return await flatConfigsToRulesDTS(configs as Parameters<typeof flatConfigsToRulesDTS>[0], {
+    includeAugmentation: false,
+  });
+}
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+function resolveInOutDir(...paths: string[]) {
+  return path.join(__dirname, '../temp/eslint-types', ...paths);
+}
 
-fs.writeFileSync(path.join(__dirname, '../src/eslint-types.d.ts'), output);
+function getDiffAsPatch(a: string, b: string) {
+  const patch = diff.createPatch('', a, b);
+  const patchDiff = patch
+    .split('\n')
+    .slice(4)
+    .map((line) => {
+      if (line.startsWith('+') && !line.startsWith('+++')) {
+        return chalk.green(line);
+      }
+      if (line.startsWith('-') && !line.startsWith('---')) {
+        return chalk.red(line);
+      }
+      if (line.startsWith('@@')) {
+        return chalk.cyan(line);
+      }
+      return line;
+    });
+  return patchDiff;
+}
