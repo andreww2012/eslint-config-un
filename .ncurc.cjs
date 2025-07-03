7472b221a9656e70b0b896c2bae097421389a141
Andrew Kazakov <andreww_dev@proton.me>
2025-07-03 16:50:02 +0300

    chore(npm-check-updates): ignore old angular plugins when checking updates

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,15 +1,24 @@
 // @ts-check
 
-const IGNORED_MAJOR_VERSION_TRANSITIONS = new Set(['@types/node']);
+const IGNORED_MAJOR_VERSION_TRANSITIONS = new Set([
+  '@types/node',
+  'angular-eslint-plugin-template15',
+  'angular-eslint-plugin-template17',
+  'angular-eslint-plugin18',
+]);
 
 /**
  * @type {import('npm-check-updates').RunOptions}
  */
 module.exports = {
-  filterResults: (packageName, {currentVersionSemver, upgradedVersionSemver}) =>
+  filterResults: (packageName, {currentVersionSemver, upgradedVersionSemver, upgradedVersion}) => {
+    const currentMajorVersion = currentVersionSemver.find((v) => 'major' in v)?.major;
+    const upgradedMajorVersion =
+      upgradedVersionSemver?.major || upgradedVersion.split('@').at(-1)?.split('.')[0];
     // eslint-disable-next-line de-morgan/no-negated-conjunction
-    !(
-      currentVersionSemver[0]?.major !== upgradedVersionSemver?.major &&
-      IGNORED_MAJOR_VERSION_TRANSITIONS.has(packageName)
-    ),
+    return !(
+      IGNORED_MAJOR_VERSION_TRANSITIONS.has(packageName) &&
+      currentMajorVersion !== upgradedMajorVersion
+    );
+  },
 };
