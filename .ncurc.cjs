349093c46f121167a506ee0b9924db96e6796d92
Andrew Kazakov <andreww_dev@proton.me>
2025-10-01 15:01:27 +0300

    chore(npm-check-updates): group deps by scope or type & interactive mode by default

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,6 +1,7 @@
 // @ts-check
 
 const semver = require('semver');
+const packageJson = require('./package.json');
 
 const IGNORED_RELEASE_ONLY_VERSION_TRANSITIONS = new Set(['@typescript/native-preview']);
 
@@ -11,11 +12,31 @@ const IGNORED_MAJOR_VERSION_TRANSITIONS = new Set([
   'angular-eslint-plugin18',
 ]);
 
+const PACKAGE_GROUPS = Object.entries({
+  'typescript-eslint': [null, 'typescript-eslint'],
+  'angular-eslint': [null],
+  jest: [null],
+  'html-eslint': [null],
+}).reduce(
+  (result, [groupName, packagesInGroup]) =>
+    Object.assign(
+      result,
+      Object.fromEntries(
+        packagesInGroup.map((packageInGroup) => [
+          packageInGroup == null ? `@${groupName}/*` : packageInGroup,
+          `@${groupName}`,
+        ]),
+      ),
+    ),
+  {},
+);
+
 /**
  * @type {import('npm-check-updates').RunOptions}
  */
 module.exports = {
   dep: ['prod', 'dev', 'optional'],
+
   filterResults: (
     packageName,
     {currentVersion: currentVersionRaw, upgradedVersion: upgradedVersionRaw},
@@ -43,6 +64,21 @@ module.exports = {
         JSON.stringify(currentVersionSemver.prerelease) !==
           JSON.stringify(upgradedVersionSemver.prerelease)
       )
+    );
+  },
+
+  format: ['group'],
+  interactive: true,
+  groupFunction: (name) => {
+    const [nameScope] = name.split('/');
+    return (
+      PACKAGE_GROUPS[name] ||
+      PACKAGE_GROUPS[`${nameScope}/*`] ||
+      (name in packageJson.devDependencies && !(name in packageJson.peerDependencies)
+        ? '_3 Dev dependencies'
+        : name in packageJson.peerDependencies
+          ? '_2 Peer dependencies'
+          : '_1 Direct dependencies')
     );
   },
 };
