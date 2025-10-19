2eed27987a81dd27b744dbb94bf7f712eb08e7cd
Andrew Kazakov <andreww_dev@proton.me>
2025-10-19 21:47:03 +0300

    chore(jsonc): enable config tester and use new rule became available format

--- src/configs/jsonc.ts
+++ src/configs/jsonc.ts
@@ -66,56 +66,57 @@ export const jsoncUnConfig: UnConfigFn<'json'> = async (context) => {
         },
       },
     )
-    /* Category: Main rules */
-    .addRule('auto', OFF) // >=0.8.0
-    .addRule('key-name-casing', OFF) // >=0.8.0
-    .addRule('no-bigint-literals', ERROR) // 🟣 >=0.2.0
-    .addRule('no-binary-expression', ERROR) // 🟣 >=2.0.0
-    .addRule('no-binary-numeric-literals', ERROR) // 🟣 >=1.1.0
-    .addRule('no-comments', OFF) // 🟣 (json) >=0.1.0
-    .addRule('no-escape-sequence-in-identifier', ERROR) // 🟣 >=1.1.0
-    .addRule('no-hexadecimal-numeric-literals', OFF) // 🟣(json, jsonc) >=1.1.0
-    .addRule('no-infinity', OFF) // 🟣(json, jsonc) >=1.1.0
-    .addRule('no-nan', OFF) // 🟣(json, jsonc) >=1.1.0
-    .addRule('no-number-props', ERROR) // 🟣 >=0.2.0
-    .addRule('no-numeric-separators', ERROR) // 🟣 >=0.6.0
-    .addRule('no-octal-numeric-literals', ERROR) // 🟣 >=1.1.0
-    .addRule('no-parenthesized', ERROR) // 🟣 >=2.0.0
-    .addRule('no-plus-sign', OFF) // 🟣(json, jsonc) >=1.1.0
-    .addRule('no-regexp-literals', ERROR) // 🟣 >=0.2.0
-    .addRule('no-template-literals', ERROR) // 🟣 >=0.2.0
-    .addRule('no-undefined-value', ERROR) // 🟣 >=0.2.0
-    .addRule('no-unicode-codepoint-escapes', ERROR) // 🟣 >=1.1.0
-    .addRule('sort-array-values', OFF) // >=2.2.0
-    .addRule('sort-keys', OFF) // >=0.1.0
-    .addRule('valid-json-number', OFF) // 🟣(json, jsonc) >=0.1.0
-    .addRule('vue-custom-block/no-parsing-error', ERROR) // 🟣 >=0.8.0
-    /* Category: Extension rules */
-    .addRule('array-bracket-newline', OFF) // >=0.1.0
-    .addRule('array-bracket-spacing', OFF) // >=0.1.0
-    .addRule('array-element-newline', OFF) // >=0.1.0
-    .addRule('comma-dangle', OFF) // 🟣(json) >=0.1.0
-    .addRule('comma-style', OFF) // >=0.1.0
-    .addRule('indent', OFF) // >=0.1.0
-    .addRule('key-spacing', OFF) // >=0.1.0
-    .addRule('no-dupe-keys', ERROR) // 🟣 >=0.1.0
-    .addRule('no-floating-decimal', OFF) // 🟣(json, jsonc) >=0.9.0
-    .addRule('no-irregular-whitespace', ERROR) // >=2.5.0
-    .addRule('no-multi-str', OFF) // 🟣(json, jsonc) >=0.1.0
-    .addRule('no-octal-escape', OFF) // >=0.1.0
-    .addRule('no-octal', ERROR) // 🟣 >=1.1.0
-    .addRule('no-sparse-arrays', ERROR) // 🟣 >=0.2.0
-    .addRule('no-useless-escape', ERROR) // 🟣 >=0.1.0
-    .addRule('object-curly-newline', OFF) // >=0.1.0
-    .addRule('object-curly-spacing', OFF) // >=0.1.0
-    .addRule('object-property-newline', OFF) // >=0.1.0
-    .addRule('quote-props', OFF) // 🟣(json, jsonc) >=0.1.0
-    .addRule('quotes', OFF) // 🟣(json, jsonc) >=0.1.0
-    .addRule('space-unary-ops', ERROR) // 🟣 >=0.2.0
+    .markCategory('Main rules')
+    .addRule('auto', OFF) /** @since 0.8.0 */
+    .addRule('key-name-casing', OFF) /** @since 0.8.0 */
+    .addRule('no-bigint-literals', ERROR) /** @since 0.2.0 */ // 🟣
+    .addRule('no-binary-expression', ERROR) /** @since 2.0.0 */ // 🟣
+    .addRule('no-binary-numeric-literals', ERROR) /** @since 1.1.0 */ // 🟣
+    .addRule('no-comments', OFF) /** @since 0.1.0 */ // 🟣 (json)
+    .addRule('no-escape-sequence-in-identifier', ERROR) /** @since 1.1.0 */ // 🟣
+    .addRule('no-hexadecimal-numeric-literals', OFF) /** @since 1.1.0 */ // 🟣(json, jsonc)
+    .addRule('no-infinity', OFF) /** @since 1.1.0 */ // 🟣(json, jsonc)
+    .addRule('no-nan', OFF) /** @since 1.1.0 */ // 🟣(json, jsonc)
+    .addRule('no-number-props', ERROR) /** @since 0.2.0 */ // 🟣
+    .addRule('no-numeric-separators', ERROR) /** @since 0.6.0 */ // 🟣
+    .addRule('no-octal-numeric-literals', ERROR) /** @since 1.1.0 */ // 🟣
+    .addRule('no-parenthesized', ERROR) /** @since 2.0.0 */ // 🟣
+    .addRule('no-plus-sign', OFF) /** @since 1.1.0 */ // 🟣(json, jsonc)
+    .addRule('no-regexp-literals', ERROR) /** @since 0.2.0 */ // 🟣
+    .addRule('no-template-literals', ERROR) /** @since 0.2.0 */ // 🟣
+    .addRule('no-undefined-value', ERROR) /** @since 0.2.0 */ // 🟣
+    .addRule('no-unicode-codepoint-escapes', ERROR) /** @since 1.1.0 */ // 🟣
+    .addRule('sort-array-values', OFF) /** @since 2.2.0 */
+    .addRule('sort-keys', OFF) /** @since 0.1.0 */
+    .addRule('valid-json-number', OFF) /** @since 0.1.0 */ // 🟣(json, jsonc)
+    .addRule('vue-custom-block/no-parsing-error', ERROR) /** @since 0.8.0 */ // 🟣
+    .markCategory('Extension rules')
+    .addRule('array-bracket-newline', OFF) /** @since 0.1.0 */
+    .addRule('array-bracket-spacing', OFF) /** @since 0.1.0 */
+    .addRule('array-element-newline', OFF) /** @since 0.1.0 */
+    .addRule('comma-dangle', OFF) /** @since 0.1.0 */ // 🟣(json)
+    .addRule('comma-style', OFF) /** @since 0.1.0 */
+    .addRule('indent', OFF) /** @since 0.1.0 */
+    .addRule('key-spacing', OFF) /** @since 0.1.0 */
+    .addRule('no-dupe-keys', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('no-floating-decimal', OFF) /** @since 0.9.0 */ // 🟣(json, jsonc)
+    .addRule('no-irregular-whitespace', ERROR) /** @since 2.5.0 */
+    .addRule('no-multi-str', OFF) /** @since 0.1.0 */ // 🟣(json, jsonc)
+    .addRule('no-octal', ERROR) /** @since 1.1.0 */ // 🟣
+    .addRule('no-octal-escape', OFF) /** @since 0.1.0 */
+    .addRule('no-sparse-arrays', ERROR) /** @since 0.2.0 */ // 🟣
+    .addRule('no-useless-escape', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('object-curly-newline', OFF) /** @since 0.1.0 */
+    .addRule('object-curly-spacing', OFF) /** @since 0.1.0 */
+    .addRule('object-property-newline', OFF) /** @since 0.1.0 */
+    .addRule('quote-props', OFF) /** @since 0.1.0 */ // 🟣(json, jsonc)
+    .addRule('quotes', OFF) /** @since 0.1.0 */ // 🟣(json, jsonc)
+    .addRule('space-unary-ops', ERROR) /** @since 0.2.0 */ // 🟣
     // Included in the recommended config:
     .disableAnyRule('', 'no-unused-expressions')
     .disableAnyRule('', 'no-unused-vars')
     .disableAnyRule('', 'strict')
+    .enableConfigTesterForPlugin('jsonc')
     .addOverrides();
 
   const configBuilderJson = createConfigBuilder(context, configJson, 'jsonc');
