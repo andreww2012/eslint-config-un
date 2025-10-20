ec6c4764e896f57dc888c1abc118061bd54d5b60
Andrew Kazakov <andreww_dev@proton.me>
2025-10-20 21:55:30 +0300

    chore(solid): enable config tester, add when rule became available

--- src/configs/solid.ts
+++ src/configs/solid.ts
@@ -20,47 +20,46 @@ export const solidUnConfig: UnConfigFn<'solid'> = (context) => {
 
   configBuilder
     ?.addConfig(['solid', {includeDefaultFilesAndIgnores: true}])
-    .addRule('components-return-once', ERROR) // 🟡
+    .addRule('components-return-once', ERROR) /** @since 0.6.0 */ // 🟡
     .addRule('event-handlers', WARNING, [
       {
         // Docs say "Enable for Solid < v1.6"
         warnOnSpread: (solidParsedVersion ?? Number.POSITIVE_INFINITY) < 1.6,
       },
-    ]) // 🟡
-    .addRule('imports', ERROR) // 🟡
-    .addRule('jsx-no-duplicate-props', ERROR) // 🟢
-    .addRule('jsx-no-script-url', ERROR) // 🟢
+    ]) /** @since 0.5.0 */ // 🟡
+    .addRule('imports', ERROR) /** @since 0.7.4 */ // 🟡
+    .addRule('jsx-no-duplicate-props', ERROR) /** @since 0.7.0 */ // 🟢
+    .addRule('jsx-no-script-url', ERROR) /** @since 0.7.0 */ // 🟢
     .addRule('jsx-no-undef', ERROR, [
       {
         // I haven't testing this, but in general this could be disruptive
         autoImport: false,
         typescriptEnabled: context.configsMeta.ts.enabled,
       },
-    ]) // 🟢
-    .addRule('jsx-uses-vars', ERROR) // 🟢
-    .addRule('no-array-handlers', OFF) // TODO
-    .addRule('no-destructure', ERROR) // 🟢
-    .addRule('no-innerhtml', ERROR) // 🟢
-    .addRule('no-proxy-apis', OFF)
-    .addRule('no-react-deps', ERROR) // 🟡
+    ]) /** @since 0.1.0 */ // 🟢
+    .addRule('jsx-uses-vars', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-array-handlers', OFF) /** @since 0.10.0 */ // TODO
+    .addRule('no-destructure', ERROR) /** @since 0.3.0 */ // 🟢
+    .addRule('no-innerhtml', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-proxy-apis', OFF) /** @since 0.8.0 */
+    .addRule('no-react-deps', ERROR) /** @since 0.9.0 */ // 🟡
     // "Disallow usage of React-specific className/htmlFor props, which were deprecated in v1.4.0."
     .addRule(
       'no-react-specific-props',
       (solidParsedVersion ?? Number.POSITIVE_INFINITY) < 1.4 ? WARNING : ERROR,
-    ) // 🟡
-    .addRule('no-unknown-namespaces', ERROR) // 🟢
-    .addRule('prefer-for', ERROR) // 🟢
-    .addRule('prefer-show', OFF)
-    .addRule('reactivity', ERROR) // 🟡
+    ) /** @since 0.1.0 */ // 🟡
+    .addRule('no-unknown-namespaces', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('prefer-for', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('prefer-show', OFF) /** @since 0.4.4 */
+    .addRule('reactivity', ERROR) /** @since 0.4.0 */ // 🟡
     .addRule('self-closing-comp', ERROR, [
       {
         component: 'none',
         html: 'void',
       },
-    ]) // 🟡
-    .addRule('style-prop', WARNING) // 🟡
-    /* Category: Deprecated */
-    .addRule('prefer-classlist', OFF)
+    ]) /** @since 0.7.0 */ // 🟡
+    .addRule('style-prop', WARNING) /** @since 0.1.0 */ // 🟡
+    .enableConfigTesterForPlugin('solid')
     .addOverrides();
 
   return {
