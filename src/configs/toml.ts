66b7dfde75c29f756f58ead586932fb1bd5a356f
Andrew Kazakov <andreww_dev@proton.me>
2025-10-20 22:13:23 +0300

    chore(toml): enable config tester and use new rule became available format

--- src/configs/toml.ts
+++ src/configs/toml.ts
@@ -74,31 +74,37 @@ export const tomlUnConfig: UnConfigFn<'toml'> = (context) => {
         ],
       },
     )
-    /* Category: Base rules */
-    .addRule('indent', ERROR) // 🟣 >=0.1.0
-    .addRule('keys-order', ERROR) // 🟣 >=0.1.0
-    .addRule('no-mixed-type-in-array', optionsResolved.noMixedTypeInArray ? ERROR : OFF) // >=0.1.0
+    .markCategory('Base rules')
+    .addRule('indent', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('keys-order', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule(
+      'no-mixed-type-in-array',
+      optionsResolved.noMixedTypeInArray ? ERROR : OFF,
+    ) /** @since 0.1.0 */
     .addRule('no-non-decimal-integer', ERROR, [
       {allowHexadecimal: true, ...optionsResolved.noNonDecimalIntegerExceptions},
-    ]) //  >=0.1.0
-    .addRule('no-space-dots', ERROR) // 🟣 >=0.1.0
-    .addRule('no-unreadable-number-separator', ERROR) // 🟣 >=0.1.0
-    .addRule('padding-line-between-pairs', ERROR) // 🟣 >=0.1.0
-    .addRule('padding-line-between-tables', ERROR) // 🟣 >=0.1.0
-    .addRule('precision-of-fractional-seconds', ERROR, [{max: maxPrecisionOfFractionalSeconds}]) // 🟣 >=0.1.0
-    .addRule('precision-of-integer', ERROR, [{maxBit: maxIntegerPrecisionBits}]) // 🟣 >=0.1.0
-    .addRule('quoted-keys', ERROR) // 🟣 >=0.1.0
-    .addRule('tables-order', ERROR) // 🟣 >=0.1.0
-    .addRule('vue-custom-block/no-parsing-error', ERROR) // 🟣 >=0.1.0
-    /* Category: Extension rules */
-    .addRule('array-bracket-newline', ERROR) // 🟣 >=0.1.0
-    .addRule('array-bracket-spacing', ERROR) // 🟣 >=0.1.0
-    .addRule('array-element-newline', ERROR) // 🟣 >=0.1.0
-    .addRule('comma-style', ERROR) // 🟣 >=0.1.0
-    .addRule('inline-table-curly-spacing', ERROR) // 🟣 >=0.1.0
-    .addRule('key-spacing', ERROR) // 🟣 >=0.2.0
-    .addRule('spaced-comment', ERROR) // 🟣 >=0.1.0
-    .addRule('table-bracket-spacing', ERROR) // 🟣 >=0.1.0
+    ]) /** @since 0.1.0 */
+    .addRule('no-space-dots', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('no-unreadable-number-separator', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('padding-line-between-pairs', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('padding-line-between-tables', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('precision-of-fractional-seconds', ERROR, [
+      {max: maxPrecisionOfFractionalSeconds},
+    ]) /** @since 0.1.0 */ // 🟣
+    .addRule('precision-of-integer', ERROR, [{maxBit: maxIntegerPrecisionBits}]) /** @since 0.1.0 */ // 🟣
+    .addRule('quoted-keys', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('tables-order', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('vue-custom-block/no-parsing-error', ERROR) /** @since 0.1.0 */ // 🟣
+    .markCategory('Extension rules')
+    .addRule('array-bracket-newline', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('array-bracket-spacing', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('array-element-newline', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('comma-style', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('inline-table-curly-spacing', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('key-spacing', ERROR) /** @since 0.2.0 */ // 🟣
+    .addRule('spaced-comment', ERROR) /** @since 0.1.0 */ // 🟣
+    .addRule('table-bracket-spacing', ERROR) /** @since 0.1.0 */ // 🟣
+    .enableConfigTesterForPlugin('toml')
     .addOverrides();
 
   return {
