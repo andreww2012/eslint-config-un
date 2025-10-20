839a5dcc0396f9ab60855c14d6d155acb89c5f1e
Andrew Kazakov <andreww_dev@proton.me>
2025-10-20 20:25:22 +0300

    chore(qwik): enable config tester, add when rule became available

--- src/configs/qwik.ts
+++ src/configs/qwik.ts
@@ -27,24 +27,25 @@ export const qwikUnConfig: UnConfigFn<'qwik'> = (context) => {
         filesFallback: [GLOB_JS_TS_X],
       },
     ])
+    .addRule('jsx-a', ERROR) /** @since 1.2.11 */ // 🟣
+    .addRule('jsx-img', OFF) /** @since 1.1.0 */ // 🟣
     .addRule('jsx-key', ERROR, [
       {
         checkFragmentShorthand: true, // Default: false
         // checkKeyMustBeforeSpread: true, // Doesn't do anything :)
         warnOnDuplicates: true, // Default: false
       },
-    ]) // 🟣
-    .addRule('jsx-a', ERROR) // 🟣
-    .addRule('jsx-img', OFF) // 🟣
-    .addRule('jsx-no-script-url', ERROR) // 🟣
-    .addRule('loader-location', ERROR, [{routesDir}]) // 🟣
-    .addRule('no-react-props', ERROR) // 🟢🟣
-    .addRule('no-use-visible-task', ERROR) // 🟣
-    .addRule('prefer-classlist', ERROR) // 🟣
+    ]) /** @since 0.22.0 */ // 🟣
+    .addRule('jsx-no-script-url', ERROR) /** @since 0.22.0 */ // 🟣
+    .addRule('loader-location', ERROR, [{routesDir}]) /** @since 0.17.0 */ // 🟣
+    .addRule('no-react-props', ERROR) /** @since 0.22.0 */ // 🟢🟣
+    .addRule('no-use-visible-task', ERROR) /** @since 1.3.0 */ // 🟣
+    .addRule('prefer-classlist', ERROR) /** @since 0.22.0 */ // 🟣
     // TODO not sure if this is useful - `no-unused-vars` should catch the same problems?
-    .addRule('unused-server', ERROR) // 🟢🟣
-    .addRule('use-method-usage', ERROR) // 🟢🟣
-    .addRule('valid-lexical-scope', ERROR) // 🟢🟣
+    .addRule('unused-server', ERROR) /** @since 0.24.0 */ // 🟢🟣
+    .addRule('use-method-usage', ERROR) /** @since 0.102.0 */ // 🟢🟣
+    .addRule('valid-lexical-scope', ERROR) /** @since 0.0.26 */ // 🟢🟣
+    .enableConfigTesterForPlugin('qwik')
     .addOverrides();
 
   return {
