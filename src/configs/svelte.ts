c3b97b5e8c4d95ea10b2aca229f9401277690188
Andrew Kazakov <andreww_dev@proton.me>
2025-10-20 22:02:57 +0300

    chore(svelte): enable config tester and fix problems

--- src/configs/svelte.ts
+++ src/configs/svelte.ts
@@ -1,6 +1,6 @@
 import type {Config as SvelteKitConfig} from '@sveltejs/kit';
 import {ERROR, GLOB_SVELTE, OFF, WARNING} from '../constants';
-import {type UnConfigOptions, createConfigBuilder} from '../eslint';
+import {type UnConfigOptions, createConfigBuilder, type RuleNamesForPlugin} from '../eslint';
 import {pluginsLoaders} from '../plugins';
 import {assignDefaults, doesPackageExist, getKeysOfTruthyValues, interopDefault} from '../utils';
 import {noRestrictedHtmlElementsDefault} from './shared';
@@ -93,6 +93,11 @@ const LATEST_SVELTE_MAJOR_VERSION = 5;
 const DEFAULT_SVELTE_FILES: string[] = [GLOB_SVELTE];
 const DEFAULT_SVELTE_SCRIPT_FILES: string[] = ['**/*.svelte.{js,ts}'];
 
+const SVELTE_SYSTEM_RULES = new Set<string>([
+  'comment-directive',
+  'system',
+] satisfies RuleNamesForPlugin<'svelte'>[]);
+
 export const svelteUnConfig: UnConfigFn<'svelte'> = async (context) => {
   const [eslintPluginSvelte, {parser: typescriptEslintParser}] = await Promise.all([
     pluginsLoaders.svelte(context).then(({module}) => module),
@@ -156,11 +161,16 @@ export const svelteUnConfig: UnConfigFn<'svelte'> = async (context) => {
         }),
       },
     )
-    .addRule('comment-directive', ERROR, [{reportUnusedDisableDirectives: true}]) // 🟢 >=0.0.13
+    .addRule('comment-directive', ERROR, [
+      {reportUnusedDisableDirectives: true},
+    ]) /** @since 0.0.13 */ // 🟢
     // "This rule is a system rule for working the this plugin. This rule does not report any errors, but make sure the rule is enabled for the this plugin to work properly"
-    .addRule('system', ERROR) // 🟢 >=0.0.13
+    .addRule('system', ERROR) /** @since 0.0.13 */ // 🟢
     // Crashes on `statement.expression.type` (`expression` is null)
-    .disableAnyRule('sonarjs', 'no-unused-collection');
+    .disableAnyRule('sonarjs', 'no-unused-collection')
+    .enableConfigTesterForPlugin('svelte', {
+      rulesToSkipInConfig: (ruleName) => !SVELTE_SYSTEM_RULES.has(ruleName),
+    });
 
   // Legend:
   // 🟢 - in recommended
@@ -172,72 +182,74 @@ export const svelteUnConfig: UnConfigFn<'svelte'> = async (context) => {
       'svelte',
       {includeDefaultFilesAndIgnores: true, filesFallback: DEFAULT_SVELTE_FILES},
     ])
-    /* CATEGORY: Possible Errors */
-    .addRule('infinite-reactive-loop', ERROR) // 🟢4️⃣ >=2.16.0
-    .addRule('no-dom-manipulating', ERROR) // 🟢 >=2.13.0
-    .addRule('no-dupe-else-if-blocks', ERROR) // 🟢 >=0.0.1
-    .addRule('no-dupe-on-directives', ERROR) // 🟢4️⃣ >=2.14.0
-    .addRule('no-dupe-style-properties', ERROR) // 🟢 >=0.31.0
-    .addRule('no-dupe-use-directives', ERROR) // 🟢 >=2.14.0
-    .addRule('no-not-function-handler', ERROR) // 🟢 >=0.5.0
-    .addRule('no-object-in-text-mustaches', ERROR) // 🟢 >=0.5.0
-    .addRule('no-raw-special-elements', ERROR) // 🟢 >=3.0.0-next.1
-    .addRule('no-reactive-reassign', ERROR) // 🟢4️⃣ >=2.27.0
-    .addRule('no-shorthand-style-property-overrides', ERROR) // 🟢 >=0.31.0
-    .addRule('no-store-async', ERROR) // 🟢 >=2.7.0
-    .addRule('no-top-level-browser-globals', ERROR) // 🟢 >=3.8.0
-    .addRule('no-unknown-style-directive-property', ERROR) // 🟢 >=0.31.0
-    .addRule('prefer-svelte-reactivity', ERROR) // 🟢 >=3.11.0
-    .addRule('require-store-callbacks-use-set-param', ERROR) // >=2.12.0
-    .addRule('require-store-reactive-access', ERROR) // 🟢 >=2.12.0
-    .addRule('valid-compile', ERROR) // >=0.7.0
-    .addRule('valid-style-parse', ERROR) // >=3.0.0
-    /* CATEGORY: Security Vulnerability */
-    .addRule('no-at-html-tags', ERROR) // 🟢 >=0.0.1
+    .markCategory('Possible Errors')
+    .addRule('infinite-reactive-loop', ERROR) /** @since 2.16.0 */ // 🟢4️⃣
+    .addRule('no-dom-manipulating', ERROR) /** @since 2.13.0 */ // 🟢
+    .addRule('no-dupe-else-if-blocks', ERROR) /** @since 0.0.1 */ // 🟢
+    .addRule('no-dupe-on-directives', ERROR) /** @since 2.14.0 */ // 🟢4️⃣
+    .addRule('no-dupe-style-properties', ERROR) /** @since 0.31.0 */ // 🟢
+    .addRule('no-dupe-use-directives', ERROR) /** @since 2.14.0 */ // 🟢
+    .addRule('no-not-function-handler', ERROR) /** @since 0.5.0 */ // 🟢
+    .addRule('no-object-in-text-mustaches', ERROR) /** @since 0.5.0 */ // 🟢
+    .addRule('no-raw-special-elements', ERROR) /** @since 3.0.0-next.1 */ // 🟢
+    .addRule('no-reactive-reassign', ERROR) /** @since 2.27.0 */ // 🟢4️⃣
+    .addRule('no-shorthand-style-property-overrides', ERROR) /** @since 0.31.0 */ // 🟢
+    .addRule('no-store-async', ERROR) /** @since 2.7.0 */ // 🟢
+    .addRule('no-top-level-browser-globals', ERROR) /** @since 3.8.0 */ // 🟢
+    .addRule('no-unknown-style-directive-property', ERROR) /** @since 0.31.0 */ // 🟢
+    .addRule('prefer-svelte-reactivity', ERROR) /** @since 3.11.0 */ // 🟢
+    .addRule('require-store-callbacks-use-set-param', ERROR) /** @since 2.12.0 */
+    .addRule('require-store-reactive-access', ERROR) /** @since 2.12.0 */ // 🟢
+    .addRule('valid-compile', ERROR) /** @since 0.7.0 */
+    .addRule('valid-style-parse', ERROR) /** @since 3.0.0 */
+    .markCategory('Security Vulnerability')
+    .addRule('no-at-html-tags', ERROR) /** @since 0.0.1 */ // 🟢
     // TODO should also set to `off` in `react` config, like in `vue`?
-    .addRule('no-target-blank', OFF) // >=0.0.4
-    /* CATEGORY: Best Practices */
+    .addRule('no-target-blank', OFF) /** @since 0.0.4 */
+    .markCategory('Best Practices')
     .addRule('block-lang', ERROR, [
       {
         script: ['ts', ...(enforceTypescriptInScriptSection ? [] : [null])],
         style: ['scss', null],
       },
-    ]) // >=2.18.0
-    .addRule('button-has-type', ERROR) // >=0.0.4
-    .addRule('no-add-event-listener', ERROR) // >=3.6.0
-    .addRule('no-at-debug-tags', ERROR) // 🟢 >=0.0.1
-    .addRule('no-ignored-unsubscribe', ERROR) // >=2.34.0
-    .addRule('no-immutable-reactive-statements', ERROR) // 🟢 >=2.27.0
-    .addRule('no-inline-styles', OFF) // >=2.35.0
-    .addRule('no-inspect', ERROR) // 🟢 >=2.45.0
-    .addRule('no-reactive-functions', ERROR) // 🟢 >=2.5.0
-    .addRule('no-reactive-literals', ERROR) // 🟢 >=2.4.0
-    .addRule('no-svelte-internal', ERROR) // 🟢 >=2.39.0
-    .addRule('no-unnecessary-state-wrap', ERROR) // 🟢 >=3.2.0
-    .addRule('no-unused-class-name', OFF) // >=2.31.0
-    .addRule('no-unused-props', ERROR) // 🟢💭 >=3.2.0
-    .addRule('no-unused-svelte-ignore', ERROR) // 🟢 >=0.19.0
-    .addRule('no-useless-children-snippet', ERROR) // 🟢 >=3.0.0-next.9
+    ]) /** @since 2.18.0 */
+    .addRule('button-has-type', ERROR) /** @since 0.0.4 */
+    .addRule('no-add-event-listener', ERROR) /** @since 3.6.0 */
+    .addRule('no-at-debug-tags', ERROR) /** @since 0.0.1 */ // 🟢
+    .addRule('no-ignored-unsubscribe', ERROR) /** @since 2.34.0 */
+    .addRule('no-immutable-reactive-statements', ERROR) /** @since 2.27.0 */ // 🟢
+    .addRule('no-inline-styles', OFF) /** @since 2.35.0 */
+    .addRule('no-inspect', ERROR) /** @since 2.45.0 */ // 🟢
+    .addRule('no-reactive-functions', ERROR) /** @since 2.5.0 */ // 🟢
+    .addRule('no-reactive-literals', ERROR) /** @since 2.4.0 */ // 🟢
+    .addRule('no-svelte-internal', ERROR) /** @since 2.39.0 */ // 🟢
+    .addRule('no-unnecessary-state-wrap', ERROR) /** @since 3.2.0 */ // 🟢
+    .addRule('no-unused-class-name', OFF) /** @since 2.31.0 */
+    .addRule('no-unused-props', ERROR) /** @since 3.2.0 */ // 🟢💭
+    .addRule('no-unused-svelte-ignore', ERROR) /** @since 0.19.0 */ // 🟢
+    .addRule('no-useless-children-snippet', ERROR) /** @since 3.0.0-next.9 */ // 🟢
     .addRule('no-useless-mustaches', ERROR, [
       {ignoreIncludesComment: true, ignoreStringEscape: true},
-    ]) // 🟢 >=0.0.4
+    ]) /** @since 0.0.4 */ // 🟢
     // "This rule reports the same as the base ESLint `prefer-const` rule, except that ignores Svelte reactive values such as `$derived` and `$props` as default. If this rule is active, make sure to disable the base `prefer-const` rule, as it will conflict with this rule."
-    .addRule('prefer-const', ERROR, [{destructuring: 'all', ignoreReadBeforeAssign: true}]) // >=3.0.0-next.6
+    .addRule('prefer-const', ERROR, [
+      {destructuring: 'all', ignoreReadBeforeAssign: true},
+    ]) /** @since 3.0.0-next.6 */
     .disableAnyRule('', 'prefer-const')
-    .addRule('prefer-destructured-store-props', OFF) // >=2.10.0
-    .addRule('prefer-writable-derived', ERROR) // >=3.6.0
-    .addRule('require-each-key', ERROR) // 🟢 >=2.28.0
-    .addRule('require-event-dispatcher-types', ERROR) // 🟢 >=2.16.0
-    .addRule('require-optimized-style-attribute', ERROR) // >=0.32.0
-    .addRule('require-stores-init', ERROR) // >=2.5.0
-    .addRule('valid-each-key', ERROR) // 🟢 >=2.28.0
-    /* CATEGORY: Stylistic Issues */
-    .addRule('consistent-selector-style', OFF) // >=3.0.0-next.15
-    .addRule('derived-has-same-inputs-outputs', WARNING) // >=2.8.0
-    .addRule('first-attribute-linebreak', OFF) // 💅 >=0.6.0
-    .addRule('html-closing-bracket-new-line', OFF) // 💅 >=2.45.0
-    .addRule('html-closing-bracket-spacing', ERROR) // 💅 >=2.3.0
-    .addRule('html-quotes', ERROR) // 💅 >=0.5.0
+    .addRule('prefer-destructured-store-props', OFF) /** @since 2.10.0 */
+    .addRule('prefer-writable-derived', ERROR) /** @since 3.6.0 */
+    .addRule('require-each-key', ERROR) /** @since 2.28.0 */ // 🟢
+    .addRule('require-event-dispatcher-types', ERROR) /** @since 2.16.0 */ // 🟢
+    .addRule('require-optimized-style-attribute', ERROR) /** @since 0.32.0 */
+    .addRule('require-stores-init', ERROR) /** @since 2.5.0 */
+    .addRule('valid-each-key', ERROR) /** @since 2.28.0 */ // 🟢
+    .markCategory('Stylistic Issues')
+    .addRule('consistent-selector-style', OFF) /** @since 3.0.0-next.15 */
+    .addRule('derived-has-same-inputs-outputs', WARNING) /** @since 2.8.0 */
+    .addRule('first-attribute-linebreak', OFF) /** @since 0.6.0 */ // 💅
+    .addRule('html-closing-bracket-new-line', OFF) /** @since 2.45.0 */ // 💅
+    .addRule('html-closing-bracket-spacing', ERROR) /** @since 2.3.0 */ // 💅
+    .addRule('html-quotes', ERROR) /** @since 0.5.0 */ // 💅
     .addRule('html-self-closing', ERROR, [
       {
         void: 'always', // default: always
@@ -247,11 +259,11 @@ export const svelteUnConfig: UnConfigFn<'svelte'> = async (context) => {
         component: 'never', // always
         svelte: 'always', // always
       },
-    ]) // 💅 >=2.5.0
-    .addRule('indent', OFF) // 💅 >=0.3.0
-    .addRule('max-attributes-per-line', OFF) // 💅 >=0.2.0
-    .addRule('mustache-spacing', ERROR) // 💅 >=0.15.0
-    .addRule('no-extra-reactive-curlies', ERROR) // >=2.4.0
+    ]) /** @since 2.5.0 */ // 💅
+    .addRule('indent', OFF) /** @since 0.3.0 */ // 💅
+    .addRule('max-attributes-per-line', OFF) /** @since 0.2.0 */ // 💅
+    .addRule('mustache-spacing', ERROR) /** @since 0.15.0 */ // 💅
+    .addRule('no-extra-reactive-curlies', ERROR) /** @since 2.4.0 */
     .addRule(
       'no-restricted-html-elements',
       ERROR,
@@ -262,34 +274,32 @@ export const svelteUnConfig: UnConfigFn<'svelte'> = async (context) => {
         },
         true,
       ),
-    ) // >=2.31.0
-    .addRule('no-spaces-around-equal-signs-in-attribute', ERROR) // 💅 >=2.3.0
-    .addRule('prefer-class-directive', ERROR) // >=0.0.1
+    ) /** @since 2.31.0 */
+    .addRule('no-spaces-around-equal-signs-in-attribute', ERROR) /** @since 2.3.0 */ // 💅
+    .addRule('prefer-class-directive', ERROR) /** @since 0.0.1 */
     // "Style directive were added in Svelte v3.46"
-    .addRule('prefer-style-directive', svelteVersion >= 3.46 ? WARNING : OFF) // >=0.22.0
-    .addRule('require-event-prefix', svelteVersion >= 5 ? ERROR : OFF) // 💅 >=3.6.0
-    .addRule('shorthand-attribute', ERROR) // 💅 >=0.5.0
-    .addRule('shorthand-directive', ERROR) // 💅 >=0.24.0
-    .addRule('sort-attributes', ERROR) // >=2.4.0
-    .addRule('spaced-html-comment', ERROR) // >=0.0.1 Yes, not supported by `prettier-plugin-svelte`
-    /* CATEGORY: Extension Rules */
-    .addRule('no-inner-declarations', ERROR) // 🟢 >=0.0.8
+    .addRule('prefer-style-directive', svelteVersion >= 3.46 ? WARNING : OFF) /** @since 0.22.0 */
+    .addRule('require-event-prefix', svelteVersion >= 5 ? ERROR : OFF) /** @since 3.6.0 */ // 💅
+    .addRule('shorthand-attribute', ERROR) /** @since 0.5.0 */ // 💅
+    .addRule('shorthand-directive', ERROR) /** @since 0.24.0 */ // 💅
+    .addRule('sort-attributes', ERROR) /** @since 2.4.0 */
+    .addRule('spaced-html-comment', ERROR) /** @since 0.0.1 */ // Yes, not supported by `prettier-plugin-svelte`
+    .markCategory('Extension Rules')
+    .addRule('no-inner-declarations', ERROR) /** @since 0.0.8 */ // 🟢
     .disableAnyRule('', 'no-inner-declarations')
-    .addRule('no-trailing-spaces', OFF) // 💅 >=2.7.0
-    /* CATEGORY: SvelteKit */
-    .addRule('no-export-load-in-svelte-module-in-kit-pages', ERROR) // 🟢 >=2.12.0
-    .addRule('no-navigation-without-resolve', ERROR) // 🟢 >=3.12.0
-    .addRule('valid-prop-names-in-kit-pages', ERROR) // 🟢 >=2.12.0
-    /* CATEGORY: Experimental */
-    .addRule('experimental-require-slot-types', OFF) // >=2.18.0
-    .addRule('experimental-require-strict-events', OFF) // >=2.18.0
-    /* CATEGORY: System */
+    .addRule('no-trailing-spaces', OFF) /** @since 2.7.0 */ // 💅
+    .markCategory('SvelteKit')
+    .addRule('no-export-load-in-svelte-module-in-kit-pages', ERROR) /** @since 2.12.0 */ // 🟢
+    .addRule('no-navigation-without-resolve', ERROR) /** @since 3.12.0 */ // 🟢
+    .addRule('valid-prop-names-in-kit-pages', ERROR) /** @since 2.12.0 */ // 🟢
+    .markCategory('Experimental')
+    .addRule('experimental-require-slot-types', OFF) /** @since 2.18.0 */
+    .addRule('experimental-require-strict-events', OFF) /** @since 2.18.0 */
+    .markCategory('System')
     // Added in the setup config
-    /* CATEGORY: Deprecated */
-    .addRule('@typescript-eslint/no-unnecessary-condition', OFF) // >=2.9.0
-    .addRule('no-dynamic-slot-name', OFF) // >=0.14.0
-    .addRule('no-goto-without-base', OFF) // >=2.36.0-next.9
-    .addRule('no-navigation-without-base', OFF) // >=2.36.0-next.9
+    .enableConfigTesterForPlugin('svelte', {
+      rulesToSkipInConfig: (ruleName) => SVELTE_SYSTEM_RULES.has(ruleName),
+    })
     .addOverrides();
 
   if (isPrettierPluginSvelteUsed) {
