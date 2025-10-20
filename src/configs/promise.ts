7409854b7c39b42a3051a3a28350ebb7a260d581
Andrew Kazakov <andreww_dev@proton.me>
2025-10-20 16:43:00 +0300

    chore(promise): enable config tester, add when rule became available

--- src/configs/promise.ts
+++ src/configs/promise.ts
@@ -17,23 +17,26 @@ export const promiseUnConfig: UnConfigFn<'promise'> = (context) => {
 
   configBuilder
     ?.addConfig(['promise', {includeDefaultFilesAndIgnores: true, doNotIgnoreHtml: true}])
-    .addRule('always-return', ERROR, [{ignoreLastCallback: true}]) // 🟢
-    .addRule('avoid-new', OFF)
-    .addRule('catch-or-return', ERROR, [{allowThenStrict: true, allowFinally: true}]) // 🟢
-    .addRule('no-callback-in-promise', ERROR) // 🟡
-    .addRule('no-multiple-resolved', WARNING)
-    .addRule('no-native', OFF)
-    .addRule('no-nesting', WARNING) // 🟡
-    .addRule('no-new-statics', ERROR) // 🟢
-    .addRule('no-promise-in-callback', WARNING) // 🟡
-    .addRule('no-return-in-finally', ERROR) // 🟡
-    .addRule('no-return-wrap', ERROR, [{allowReject: true}]) // 🟢
-    .addRule('param-names', ERROR) // 🟢
-    .addRule('prefer-await-to-callbacks', OFF)
-    .addRule('prefer-await-to-then', OFF)
-    .addRule('prefer-catch', ERROR)
-    .addRule('spec-only', ERROR)
-    .addRule('valid-params', ERROR) // 🟡
+    .addRule('always-return', ERROR, [{ignoreLastCallback: true}]) /** @since 1.0.0 */ // 🟢
+    .addRule('avoid-new', OFF) /** @since 3.4.0 */
+    .addRule('catch-or-return', ERROR, [
+      {allowThenStrict: true, allowFinally: true},
+    ]) /** @since 1.1.0 */ // 🟢
+    .addRule('no-callback-in-promise', ERROR) /** @since 3.4.0 */ // 🟡
+    .addRule('no-multiple-resolved', WARNING) /** @since 6.1.0 */
+    .addRule('no-native', OFF) /** @since 1.3.0 */
+    .addRule('no-nesting', WARNING) /** @since 3.4.0 */ // 🟡
+    .addRule('no-new-statics', ERROR) /** @since 3.7.0 */ // 🟢
+    .addRule('no-promise-in-callback', WARNING) /** @since 3.4.0 */ // 🟡
+    .addRule('no-return-in-finally', ERROR) /** @since 3.6.0 */ // 🟡
+    .addRule('no-return-wrap', ERROR, [{allowReject: true}]) /** @since 3.2.0 */ // 🟢
+    .addRule('param-names', ERROR) /** @since 1.0.0 */ // 🟢
+    .addRule('prefer-await-to-callbacks', OFF) /** @since 3.3.0 */
+    .addRule('prefer-await-to-then', OFF) /** @since 3.3.0 */
+    .addRule('prefer-catch', ERROR) /** @since 7.2.0 */
+    .addRule('spec-only', ERROR) /** @since 7.1.0 */
+    .addRule('valid-params', ERROR) /** @since 3.7.0 */ // 🟡
+    .enableConfigTesterForPlugin('promise')
     .addOverrides();
 
   return {
