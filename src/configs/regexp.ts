8155c3658f9c26aa1af9fbad9e2c9dae3e3663e1
Andrew Kazakov <andreww_dev@proton.me>
2025-10-20 21:23:11 +0300

    chore(regexp): enable config tester, add when rule became available

--- src/configs/regexp.ts
+++ src/configs/regexp.ts
@@ -17,106 +17,107 @@ export const regexpUnConfig: UnConfigFn<'regexp'> = (context) => {
 
   configBuilder
     ?.addConfig(['regexp', {includeDefaultFilesAndIgnores: true, doNotIgnoreHtml: true}])
-    /* Category: Possible Errors */
-    .addRule('no-contradiction-with-assertion', ERROR) // 🟢
+    .markCategory('Possible Errors')
+    .addRule('no-contradiction-with-assertion', ERROR) /** @since 1.2.0 */ // 🟢
     // "This rule is inspired by the `no-control-regex` rule. The positions of reports are improved over the core rule and suggestions are provided in some cases"
     .disableAnyRule('', 'no-control-regex') // TODO 🟢(enabled?!)
-    .addRule('no-control-character', ERROR)
-    .addRule('no-dupe-disjunctions', ERROR) // 🟢
-    .addRule('no-empty-alternative', ERROR) // 🟡
-    .addRule('no-empty-capturing-group', ERROR) // 🟢
+    .addRule('no-control-character', ERROR) /** @since 1.2.0 */
+    .addRule('no-dupe-disjunctions', ERROR) /** @since 0.4.0 */ // 🟢
+    .addRule('no-empty-alternative', ERROR) /** @since 0.8.0 */ // 🟡
+    .addRule('no-empty-capturing-group', ERROR) /** @since 0.12.0 */ // 🟢
     // "The reports for this rule include reports for the ESLint core no-empty-character-class rule. That is, if you use this rule, you can turn off the ESLint core no-empty-character-class rule"
-    .addRule('no-empty-character-class', ERROR) // 🟢
+    .addRule('no-empty-character-class', ERROR) /** @since 1.2.0 */ // 🟢
     .disableAnyRule('', 'no-empty-character-class') // 🟢
-    .addRule('no-empty-group', ERROR) // 🟢
-    .addRule('no-empty-lookarounds-assertion', ERROR) // 🟢
-    .addRule('no-escape-backspace', ERROR) // 🟢
-    .addRule('no-invalid-regexp', ERROR) // 🟢
+    .addRule('no-empty-group', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-empty-lookarounds-assertion', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-escape-backspace', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-invalid-regexp', ERROR) /** @since 1.0.0 */ // 🟢
     .disableAnyRule('', 'no-invalid-regexp')
-    .addRule('no-lazy-ends', ERROR) // 🟡
-    .addRule('no-misleading-capturing-group', ERROR) // 🟢
-    .addRule('no-misleading-unicode-character', ERROR) // 🟢
-    .addRule('no-missing-g-flag', ERROR) // 🟢
-    .addRule('no-optional-assertion', ERROR) // 🟢
-    .addRule('no-potentially-useless-backreference', WARNING) // 🟡
-    .addRule('no-super-linear-backtracking', ERROR) // 🟢
-    .addRule('no-super-linear-move', OFF)
-    .addRule('no-useless-assertions', ERROR) // 🟢
+    .addRule('no-lazy-ends', ERROR) /** @since 0.8.0 */ // 🟡
+    .addRule('no-misleading-capturing-group', ERROR) /** @since 1.12.0 */ // 🟢
+    .addRule('no-misleading-unicode-character', ERROR) /** @since  1.2.0 */ // 🟢
+    .addRule('no-missing-g-flag', ERROR) /** @since 1.10.0 */ // 🟢
+    .addRule('no-optional-assertion', ERROR) /** @since 0.9.0 */ // 🟢
+    .addRule('no-potentially-useless-backreference', WARNING) /** @since 0.9.0 */ // 🟡
+    .addRule('no-super-linear-backtracking', ERROR) /** @since 0.13.0 */ // 🟢
+    .addRule('no-super-linear-move', OFF) /** @since 0.13.0 */
+    .addRule('no-useless-assertions', ERROR) /** @since 0.9.0 */ // 🟢
     // "This rule is a based on the ESLint core `no-useless-backreference` rule. It reports all the ESLint core rule reports and some more"
-    .addRule('no-useless-backreference', ERROR) // 🟢
+    .addRule('no-useless-backreference', ERROR) /** @since 0.1.0 */ // 🟢
     .disableAnyRule('', 'no-useless-backreference') // 🟢
-    .addRule('no-useless-dollar-replacements', ERROR) // 🟢
-    .addRule('strict', ERROR) // 🟢
-    /* Category: Best Practices */
-    .addRule('confusing-quantifier', ERROR) // 🟡
-    .addRule('control-character-escape', ERROR) // 🟢
-    .addRule('negation', ERROR) // 🟢
-    .addRule('no-dupe-characters-character-class', ERROR) // 🟢
-    .addRule('no-empty-string-literal', ERROR) // 🟢
-    .addRule('no-extra-lookaround-assertions', ERROR) // 🟢
-    .addRule('no-invisible-character', ERROR) // 🟢
-    .addRule('no-legacy-features', ERROR) // 🟢
-    .addRule('no-non-standard-flag', ERROR) // 🟢
-    .addRule('no-obscure-range', WARNING) // 🟢
-    .addRule('no-octal', ERROR)
-    .addRule('no-standalone-backslash', ERROR)
-    .addRule('no-trivially-nested-assertion', ERROR) // 🟢
-    .addRule('no-trivially-nested-quantifier', ERROR) // 🟢
-    .addRule('no-unused-capturing-group', ERROR) // 🟢
-    .addRule('no-useless-character-class', ERROR) // 🟢
-    .addRule('no-useless-flag', ERROR) // 🟡
-    .addRule('no-useless-lazy', ERROR) // 🟢
-    .addRule('no-useless-quantifier', ERROR) // 🟢
-    .addRule('no-useless-range', ERROR) // 🟢
-    .addRule('no-useless-set-operand', ERROR) // 🟢
-    .addRule('no-useless-string-literal', ERROR) // 🟢
-    .addRule('no-useless-two-nums-quantifier', ERROR) // 🟢
-    .addRule('no-zero-quantifier', ERROR) // 🟢
-    .addRule('optimal-lookaround-quantifier', ERROR) // 🟡
-    .addRule('optimal-quantifier-concatenation', ERROR) // 🟢
-    .addRule('prefer-escape-replacement-dollar-char', OFF)
-    .addRule('prefer-predefined-assertion', ERROR) // 🟢
-    .addRule('prefer-quantifier', ERROR, [{allows: ['www', String.raw`\d\d`]}])
-    .addRule('prefer-range', ERROR) // 🟢
+    .addRule('no-useless-dollar-replacements', ERROR) /** @since 0.6.0 */ // 🟢
+    .addRule('strict', ERROR) /** @since 0.12.0 */ // 🟢
+    .markCategory('Best Practices')
+    .addRule('confusing-quantifier', ERROR) /** @since 0.8.0 */ // 🟡
+    .addRule('control-character-escape', ERROR) /** @since 0.9.0 */ // 🟢
+    .addRule('negation', ERROR) /** @since 0.4.0 */ // 🟢
+    .addRule('no-dupe-characters-character-class', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-empty-string-literal', ERROR) /** @since 2.0.0-next.11 */ // 🟢
+    .addRule('no-extra-lookaround-assertions', ERROR) /** @since 1.11.0 */ // 🟢
+    .addRule('no-invisible-character', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-legacy-features', ERROR) /** @since 0.6.0 */ // 🟢
+    .addRule('no-non-standard-flag', ERROR) /** @since 0.9.0 */ // 🟢
+    .addRule('no-obscure-range', WARNING) /** @since 0.9.0 */ // 🟢
+    .addRule('no-octal', ERROR) /** @since 0.1.0 */
+    .addRule('no-standalone-backslash', ERROR) /** @since 0.10.0 */
+    .addRule('no-trivially-nested-assertion', ERROR) /** @since 0.9.0 */ // 🟢
+    .addRule('no-trivially-nested-quantifier', ERROR) /** @since 0.9.0 */ // 🟢
+    .addRule('no-unused-capturing-group', ERROR) /** @since 0.6.0 */ // 🟢
+    .addRule('no-useless-character-class', ERROR) /** @since 0.3.0 */ // 🟢
+    .addRule('no-useless-flag', ERROR) /** @since 0.9.0 */ // 🟡
+    .addRule('no-useless-lazy', ERROR) /** @since 0.10.0 */ // 🟢
+    .addRule('no-useless-quantifier', ERROR) /** @since 0.10.0 */ // 🟢
+    .addRule('no-useless-range', ERROR) /** @since 0.3.0 */ // 🟢
+    .addRule('no-useless-set-operand', ERROR) /** @since 2.0.0-next.10 */ // 🟢
+    .addRule('no-useless-string-literal', ERROR) /** @since 2.0.0-next.12 */ // 🟢
+    .addRule('no-useless-two-nums-quantifier', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-zero-quantifier', ERROR) /** @since 0.10.0 */ // 🟢
+    .addRule('optimal-lookaround-quantifier', ERROR) /** @since 0.8.0 */ // 🟡
+    .addRule('optimal-quantifier-concatenation', ERROR) /** @since 0.11.0 */ // 🟢
+    .addRule('prefer-escape-replacement-dollar-char', OFF) /** @since 0.6.0 */
+    .addRule('prefer-predefined-assertion', ERROR) /** @since 0.10.0 */ // 🟢
+    .addRule('prefer-quantifier', ERROR, [{allows: ['www', String.raw`\d\d`]}]) /** @since 0.2.0 */
+    .addRule('prefer-range', ERROR) /** @since 0.4.0 */ // 🟢
     // Same (?) as `ts/prefer-regexp-exec` which is turned off by default
-    .addRule('prefer-regexp-exec', OFF)
+    .addRule('prefer-regexp-exec', OFF) /** @since 0.3.0 */
     .disableAnyRule('unicorn', 'prefer-regexp-test')
     .addRule('prefer-regexp-test', ERROR) // TODO better than the unicorn rule? Off by default
-    .addRule('prefer-set-operation', ERROR) // 🟢
+    .addRule('prefer-set-operation', ERROR) /** @since 2.0.0-next.9 */ // 🟢
     // "This rule is inspired by the `require-unicode-regexp` rule. The position of the report is improved over the core rule and arguments of new RegExp() are also checked"
     // Yes, still off - we just want to show that it's a better replacement for the core rule
-    .addRule('require-unicode-regexp', OFF)
+    .addRule('require-unicode-regexp', OFF) /** @since 1.2.0 */
     .disableAnyRule('', 'require-unicode-regexp')
-    .addRule('require-unicode-sets-regexp', OFF)
-    .addRule('simplify-set-operations', ERROR) // 🟢
-    .addRule('sort-alternatives', OFF)
-    .addRule('use-ignore-case', ERROR) // 🟢
-    /* Category: Stylistic Issues */
-    .addRule('grapheme-string-literal', OFF) // TODO
-    .addRule('hexadecimal-escape', ERROR, ['never'])
-    .addRule('letter-case', ERROR)
-    .addRule('match-any', ERROR) // 🟢
-    .addRule('no-useless-escape', ERROR) // 🟢
-    .addRule('no-useless-non-capturing-group', ERROR) // 🟢
-    .addRule('prefer-character-class', ERROR) // 🟢
-    .addRule('prefer-d', ERROR) // 🟢
-    .addRule('prefer-lookaround', ERROR, [{lookbehind: false}])
-    .addRule('prefer-named-backreference', OFF)
+    .addRule('require-unicode-sets-regexp', OFF) /** @since 2.0.0-next.7 */
+    .addRule('simplify-set-operations', ERROR) /** @since 2.0.0-next.11 */ // 🟢
+    .addRule('sort-alternatives', OFF) /** @since 0.12.0 */
+    .addRule('use-ignore-case', ERROR) /** @since 1.4.0 */ // 🟢
+    .markCategory('Stylistic Issues')
+    .addRule('grapheme-string-literal', OFF) /** @since 2.0.0-next.13 */ // TODO
+    .addRule('hexadecimal-escape', ERROR, ['never']) /** @since 0.9.0 */
+    .addRule('letter-case', ERROR) /** @since 0.3.0 */
+    .addRule('match-any', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('no-useless-escape', ERROR) /** @since 0.4.0 */ // 🟢
+    .addRule('no-useless-non-capturing-group', ERROR) /** @since 0.4.0 */ // 🟢
+    .addRule('prefer-character-class', ERROR) /** @since 0.4.0 */ // 🟢
+    .addRule('prefer-d', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('prefer-lookaround', ERROR, [{lookbehind: false}]) /** @since 1.2.0 */
+    .addRule('prefer-named-backreference', OFF) /** @since 0.9.0 */
     // "This rule is inspired by the `prefer-named-capture-group` rule. The positions of reports are improved over the core rule and arguments of new RegExp() are also checked"
     // Yes, still off - we just want to show that it's a better replacement for the core rule
-    .addRule('prefer-named-capture-group', OFF)
+    .addRule('prefer-named-capture-group', OFF) /** @since 1.2.0 */
     .disableAnyRule('', 'prefer-named-capture-group')
-    .addRule('prefer-named-replacement', OFF)
-    .addRule('prefer-plus-quantifier', ERROR) // 🟢
-    .addRule('prefer-question-quantifier', ERROR) // 🟢
-    .addRule('prefer-result-array-groups', OFF)
-    .addRule('prefer-star-quantifier', ERROR) // 🟢
-    .addRule('prefer-unicode-codepoint-escapes', ERROR) // 🟢
-    .addRule('prefer-w', ERROR) // 🟢
-    .addRule('sort-character-class-elements', ERROR)
-    .addRule('sort-flags', ERROR) // 🟢
-    .addRule('unicode-escape', OFF)
-    .addRule('unicode-property', ERROR)
+    .addRule('prefer-named-replacement', OFF) /** @since 1.4.0 */
+    .addRule('prefer-plus-quantifier', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('prefer-question-quantifier', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('prefer-result-array-groups', OFF) /** @since 1.4.0 */
+    .addRule('prefer-star-quantifier', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('prefer-unicode-codepoint-escapes', ERROR) /** @since 0.3.0 */ // 🟢
+    .addRule('prefer-w', ERROR) /** @since 0.1.0 */ // 🟢
+    .addRule('sort-character-class-elements', ERROR) /** @since 0.12.0 */
+    .addRule('sort-flags', ERROR) /** @since 0.9.0 */ // 🟢
+    .addRule('unicode-escape', OFF) /** @since 0.9.0 */
+    .addRule('unicode-property', ERROR) /** @since 2.5.0 */
+    .enableConfigTesterForPlugin('regexp')
     .addOverrides();
 
   return {
