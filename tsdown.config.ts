909c47b7d152c5823cd8c2ea342ff257d3a7702f
Andrew Kazakov <andreww_dev@proton.me>
2025-12-08 15:48:55 +0300

    build: enable tsdown's `unbundle` mode Also unset `shims` and `hash` options because they don't change anything

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,6 @@ import {defineConfig} from 'tsdown';
 export default defineConfig({
   entry: ['src/index.ts', 'src/snippets.ts'],
   format: 'esm',
-  hash: false,
-  shims: true,
   dts: true,
   external: ['eslint-plugin-no-type-assertion', 'eslint-plugin-prettier'],
   noExternal: [
