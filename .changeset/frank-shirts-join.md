2caf282ca2beb77dd180bf43af389e7f57a1c138
Andrew Kazakov <andreww_dev@proton.me>
2025-12-18 17:25:43 +0300

    feat(nestJs): update @darraghor/eslint-plugin-nestjs-typed to v7.1.1 and enable 4 new rules

---
+++ .changeset/frank-shirts-join.md
@@ -0,0 +1,12 @@
+---
+'eslint-config-un': patch
+---
+
+nestJs: updated [`@darraghor/eslint-plugin-nestjs-typed` from v6.9.17 to v7.1.1](https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/compare/v6.9.17...v7.1.1):
+
+- 🟢 enabled [`use-injectable-provided-token`](https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/HEAD/src/docs/rules/use-injectable-provided-token.md) rule
+- 🟢 enabled [`api-operation-summary-description-capitalized`](https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/HEAD/src/docs/rules/api-operation-summary-description-capitalized.md) rule
+- 🟢 enabled [`api-property-should-have-api-extra-models`](https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/HEAD/src/docs/rules/api-property-should-have-api-extra-models.md) rule
+- 🟢 enabled [`validation-pipe-should-use-forbid-unknown`](https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/HEAD/src/docs/rules/validation-pipe-should-use-forbid-unknown.md) rule
+- 🔴 not enabled [`use-dependency-injection`](https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/HEAD/src/docs/rules/use-dependency-injection.md) rule
+- 🔴 not enabled [`use-correct-endpoint-naming-convention`](https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/HEAD/src/docs/rules/use-correct-endpoint-naming-convention.md) rule
--- src/configs/nest-js.ts
+++ src/configs/nest-js.ts
@@ -33,25 +33,30 @@ export default ((context, optionsRaw) => {
     .markCategory('Nest Modules and Dependency Injection')
     .addRule('injectable-should-be-provided', ERROR) /** @since 1.0.0 */
     .addRule('provided-injected-should-match-factory-parameters', ERROR) /** @since 1.0.0 */
+    .addRule('use-dependency-injection', OFF) /** @since 6.17.0 */ // 🔴
+    .addRule('use-injectable-provided-token', ERROR) /** @since 6.10.0 */
     .markCategory('Nest Swagger')
     .addRule('api-enum-property-best-practices', ERROR) /** @since 1.7.0 */
     .addRule('api-method-should-specify-api-operation', ERROR) /** @since 1.6.0 */
     .addRule('api-method-should-specify-api-response', ERROR) /** @since 2.0.0 */
+    .addRule('api-operation-summary-description-capitalized', ERROR) /** @since 6.2.0 */
     .addRule('api-property-matches-property-optionality', ERROR) /** @since 1.4.0 */
     .addRule('api-property-returning-array-should-set-array', ERROR) /** @since 1.7.0 */
+    .addRule('api-property-should-have-api-extra-models', ERROR) /** @since 6.12.0 */
     .addRule('controllers-should-supply-api-tags', ERROR) /** @since 1.6.0 */
     .markCategory('Preventing bugs')
     .addRule('all-properties-are-whitelisted', ERROR) /** @since 3.8.0 */
     .addRule('all-properties-have-explicit-defined', ERROR) /** @since 3.15.0 */
     .addRule('no-duplicate-decorators', ERROR) /** @since 4.3.0 */
     .addRule('param-decorator-name-matches-route-param', ERROR) /** @since 3.3.0 */
-    .addRule('should-specify-forbid-unknown-values', ERROR) /** @since 3.2.0 */
     .addRule('validate-nested-of-array-should-set-each', ERROR) /** @since 3.5.0 */
     .addRule('validated-non-primitive-property-needs-type-decorator', ERROR) /** @since 3.5.0 */
+    .addRule('validation-pipe-should-use-forbid-unknown', ERROR) /** @since 3.2.0 */ // Renamed from `should-specify-forbid-unknown-values` in v7.0.0
     .markCategory('Security')
     .addRule('api-methods-should-be-guarded', OFF) /** @since 3.19.0 */ // 🔴
     .markCategory('Code Consistency')
     .addRule('sort-module-metadata-arrays', OFF) /** @since 3.22.0 */ // 🔴
+    .addRule('use-correct-endpoint-naming-convention', OFF) /** @since 6.11.0 */ // 🔴
     .enableConfigTesterForPlugin('nestjs')
     .addOverrides();
 
