ade39d6b19b2e1fb7354457e720f81ecacc73a46
Andrew Kazakov <andreww_dev@proton.me>
2026-01-22 11:52:47 +0300

    chore: disable commitlint's `body-max-length` rule

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,6 @@ import type {UserConfig} from '@commitlint/types';
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-length': [2, 'always', 150],
     'body-max-line-length': [0],
     'header-max-length': [2, 'always', 120],
     'subject-full-stop': [0], // Sometimes full stop is used for shorthands
