265edd04e74196102d714fa6612b1f333b97a512
Andrew Kazakov <andreww_dev@proton.me>
2026-04-11 23:45:07 +0300

    test(node): increase coverage to 100%

--- test/configs/node/node.spec.ts
+++ test/configs/node/node.spec.ts
@@ -4,6 +4,7 @@ const FIXTURES = {
   importBuiltinNodeModuleWithoutNodeProtocol: 'import-builtin-node-module-without-node-protocol.js',
   packageJsonWithEnginesNodeFrom14: 'package-json-engines-node-from-14',
   packageJsonWithEnginesNodeFrom20_11: 'package-json-engines-node-from-20-11',
+  packageJsonWithoutEnginesNode: 'package-json-without-engines-node',
 } as const;
 
 const mockUserPackageJsonPath = (fixtureDirectory: string) => {
@@ -30,37 +31,58 @@ describe('basic tests', async () => {
 
     expect(nodeConfig?.languageOptions?.['globals']).toStrictEqual(globals.node);
   });
-});
 
-describe('un options', () => {
-  it('respects `overrides` and `overridesAny` in `node` eslint config', async () => {
-    const configResult = await computeEslintConfig({
-      node: {overrides: {'node/no-sync': 0}, overridesAny: {'no-console': 0}},
+  describe('mode: all configs are disabled', () => {
+    it('does not create `node` eslint config', async () => {
+      await expectConfigState({}, 'node', false);
     });
 
-    expect(
-      getRuleSeverityFromEslintRuleEntry(configResult.getRuleEntry('node', 'node/no-sync')),
-    ).toBe(0);
+    it('creates `node` eslint config if explicitly enabled', async () => {
+      await expectConfigState('node', 'node', true);
+    });
 
-    expect(
-      getRuleSeverityFromEslintRuleEntry(configResult.getRuleEntry('node', 'no-console')),
-    ).toBe(0);
+    it('does not create `node` eslint config and prints a warning if explicitly disabled', async () => {
+      await expectConfigState({node: false}, 'node', ['node', false]);
+    });
+  });
+
+  describe('mode: all configs are not explicitly enabled or disabled', () => {
+    it('creates `node` eslint config', async () => {
+      await expectConfigState({}, 'node', true, 'default');
+    });
+
+    it('creates `node` eslint config and prints a warning if explicitly enabled', async () => {
+      await expectConfigState('node', 'node', ['node', true], 'default');
+    });
+
+    it('does not create `node` eslint config if explicitly disabled', async () => {
+      await expectConfigState({node: false}, 'node', false, 'default');
+    });
+  });
+
+  describe('mode: misc configs are enabled', () => {
+    it('creates `node` eslint config', async () => {
+      await expectConfigState({}, 'node', true, 'misc-enabled');
+    });
+
+    it('creates `node` eslint config and prints a warning if explicitly enabled', async () => {
+      await expectConfigState('node', 'node', ['node', true], 'misc-enabled');
+    });
+
+    it('does not create `node` eslint config if explicitly disabled', async () => {
+      await expectConfigState({node: false}, 'node', false, 'misc-enabled');
+    });
   });
 });
 
-describe('rules', async () => {
-  const configResult = await computeEslintConfig('node');
+describe('rules', () => {
+  it('correctly sets severities by default', async () => {
+    const configResult = await computeEslintConfig('node');
 
-  it('enables `node/hashbang` rule by default', () => {
-    const ruleEntry = configResult.getRuleEntry('node', 'node/hashbang');
-
-    expect(JSON.stringify(ruleEntry)).toMatchInlineSnapshot(`"[2]"`);
-  });
-
-  it('does not enable `node/global-require` rule by default', () => {
-    const ruleEntry = configResult.getRuleEntry('node', 'node/global-require');
-
-    expect(JSON.stringify(ruleEntry)).toMatchInlineSnapshot(`"[0]"`);
+    expect(configResult.getRuleSeverities('node')).toMatchObject({
+      'node/hashbang': 2,
+      'node/global-require': 0,
+    });
   });
 
   it('`node/prefer-node-protocol` rule works', async () => {
@@ -76,7 +98,7 @@ describe('rules', async () => {
       'node/prefer-node-protocol',
     );
 
-    expect(error?.message).toMatchInlineSnapshot(`"Prefer \`node:fs\` over \`fs\`."`);
+    expect(error?.message).toMatchInlineSnapshot('"Prefer `node:fs` over `fs`."');
   });
 
   describe("`unicorn/prefer-import-meta-properties` rule based on `engines.node` in user's package.json", () => {
@@ -88,84 +110,207 @@ describe('rules', async () => {
       mockUserPackageJsonPath(FIXTURES.packageJsonWithEnginesNodeFrom20_11);
 
       const configResult = await computeEslintConfig('node');
-      const ruleEntry = configResult.getRuleEntry('node', 'unicorn/prefer-import-meta-properties');
 
-      expect(JSON.stringify(ruleEntry)).toMatchInlineSnapshot(`"[2]"`);
+      expect(
+        configResult.getRuleEntrySeverity('node', 'unicorn/prefer-import-meta-properties'),
+      ).toBe(2);
     });
 
     it('disables the rule if does not support `import.meta`', async () => {
       mockUserPackageJsonPath(FIXTURES.packageJsonWithEnginesNodeFrom14);
 
       const configResult = await computeEslintConfig('node');
-      const ruleEntry = configResult.getRuleEntry('node', 'unicorn/prefer-import-meta-properties');
 
-      expect(JSON.stringify(ruleEntry)).toMatchInlineSnapshot(`"[0]"`);
+      expect(
+        configResult.getRuleEntrySeverity('node', 'unicorn/prefer-import-meta-properties'),
+      ).toBe(0);
+    });
+
+    it('disables the rule if `engines.node` is not set in package.json', async () => {
+      mockUserPackageJsonPath(FIXTURES.packageJsonWithoutEnginesNode);
+
+      const configResult = await computeEslintConfig('node');
+
+      expect(
+        configResult.getRuleEntrySeverity('node', 'unicorn/prefer-import-meta-properties'),
+      ).toBe(0);
     });
   });
 });
 
+describe('un options', () => {
+  describe('option: `files`', () => {
+    it('uses user-provided `files` in `node` eslint config', async () => {
+      const FILES = ['src/**/*.js'];
+
+      const configResult = await computeEslintConfig({node: {files: FILES}});
+
+      expect(configResult.getConfigByUnPostfix('node')?.files).toStrictEqual(FILES);
+    });
+
+    it('disables `node` eslint config when set to empty array', async () => {
+      const configResult = await computeEslintConfig({node: {files: []}});
+
+      expect(configResult.getConfigByUnPostfix('node')).toBeUndefined();
+    });
+  });
+
+  describe('option: `ignores`', () => {
+    it('uses user-provided `ignores` in `node` eslint config and merges them with defaults', async () => {
+      const IGNORES = ['**/fixtures/**'];
+
+      const configResult = await computeEslintConfig({node: {ignores: IGNORES}});
+
+      const ignores = configResult.getConfigByUnPostfix('node')?.ignores;
+
+      expect(ignores).toIncludeAllMembers(IGNORES);
+      expect(ignores?.length).toBeGreaterThan(IGNORES.length);
+    });
+  });
+
+  it('respects `overrides` and `overridesAny` in `node` eslint config', async () => {
+    const configResult = await computeEslintConfig({
+      node: {overrides: {'node/no-sync': 0}, overridesAny: {'no-console': 0}},
+    });
+
+    expect(configResult.getRuleEntrySeverity('node', 'node/no-sync')).toBe(0);
+    expect(configResult.getRuleEntrySeverity('node', 'no-console')).toBe(0);
+  });
+});
+
 describe('options', () => {
-  describe('`settings`', async () => {
-    const PLUGIN_SETTINGS = {tsconfigPath: 'foo/tsconfig.json'};
+  describe('option: `settings`', () => {
+    it('does not set plugin settings on `node` eslint config by default', async () => {
+      const configResult = await computeEslintConfig('node');
 
-    const configResult = await computeEslintConfig({node: {settings: PLUGIN_SETTINGS}});
+      expect(configResult.getConfigByUnPostfix('node')?.settings?.['node']).toBeUndefined();
+    });
 
-    it('sets plugin settings on `node` eslint config', () => {
+    it('sets plugin settings on `node` eslint config when set', async () => {
+      const PLUGIN_SETTINGS = {tsconfigPath: 'foo/tsconfig.json'};
+      const configResult = await computeEslintConfig({node: {settings: PLUGIN_SETTINGS}});
+
       expect(configResult.getConfigByUnPostfix('node')?.settings?.['node']).toStrictEqual(
         PLUGIN_SETTINGS,
       );
     });
   });
 
-  describe('`noUnsupportedFeaturesIgnores`', async () => {
+  describe('option: `noUnsupportedFeaturesIgnores`', async () => {
     const configResult = await computeEslintConfig({
       node: {noUnsupportedFeaturesIgnores: {esBuiltins: ['Promise'], nodeBuiltins: []}},
     });
 
     it('adds ignored unsupported features to `node/no-unsupported-features/<group>` rule if `noUnsupportedFeaturesIgnores.<group>` is non-empty array', () => {
       expect(
-        JSON.stringify(
-          configResult.getRuleEntry('node', 'node/no-unsupported-features/es-builtins'),
-        ),
-      ).toMatchInlineSnapshot(`"[2,{"ignores":["Promise"]}]"`);
+        configResult.getRuleEntry('node', 'node/no-unsupported-features/es-builtins'),
+      ).toMatchInlineSnapshot('[2, {"ignores": ["Promise"]}]');
     });
 
     it('does not add ignored unsupported features to `node/no-unsupported-features/<group>` rule if `noUnsupportedFeaturesIgnores.<group>` is not set', () => {
       expect(
-        JSON.stringify(configResult.getRuleEntry('node', 'node/no-unsupported-features/es-syntax')),
-      ).toMatchInlineSnapshot(`"[2]"`);
+        configResult.getRuleEntry('node', 'node/no-unsupported-features/es-syntax'),
+      ).toMatchInlineSnapshot('[2]');
     });
 
     it('does not add ignored unsupported features to `node/no-unsupported-features/<group>` rule if `noUnsupportedFeaturesIgnores.<group>` is empty array', () => {
       expect(
-        JSON.stringify(
-          configResult.getRuleEntry('node', 'node/no-unsupported-features/node-builtins'),
-        ),
-      ).toMatchInlineSnapshot(`"[2]"`);
+        configResult.getRuleEntry('node', 'node/no-unsupported-features/node-builtins'),
+      ).toMatchInlineSnapshot('[2]');
+    });
+
+    it('adds ignored unsupported features to `node/no-unsupported-features/es-syntax` rule if `noUnsupportedFeaturesIgnores.esSyntax` is non-empty array', async () => {
+      const SYNTAX = ['modules' as const];
+
+      const result = await computeEslintConfig({
+        node: {noUnsupportedFeaturesIgnores: {esSyntax: SYNTAX}},
+      });
+
+      expect(
+        result.getRuleEntryOptions('node', 'node/no-unsupported-features/es-syntax'),
+      ).toStrictEqual([{ignores: SYNTAX}]);
+    });
+
+    it('adds ignored unsupported features to `node/no-unsupported-features/node-builtins` rule if `noUnsupportedFeaturesIgnores.nodeBuiltins` is non-empty array', async () => {
+      const NODE_BUILTINS = ['fs' as const];
+
+      const result = await computeEslintConfig({
+        node: {noUnsupportedFeaturesIgnores: {nodeBuiltins: NODE_BUILTINS}},
+      });
+
+      expect(
+        result.getRuleEntryOptions('node', 'node/no-unsupported-features/node-builtins'),
+      ).toStrictEqual([{ignores: NODE_BUILTINS}]);
     });
   });
 
-  describe('`preferGlobal`', async () => {
+  describe('option: `preferGlobal`', async () => {
     const configResult = await computeEslintConfig({
       node: {preferGlobal: {buffer: false, console: true}},
     });
 
     it('sets `node/prefer-global/<feature>` to `never` if `preferGlobal.<feature>` is set to `false`', () => {
-      expect(
-        JSON.stringify(configResult.getRuleEntry('node', 'node/prefer-global/buffer')),
-      ).toMatchInlineSnapshot(`"[2,"never"]"`);
+      expect(configResult.getRuleEntry('node', 'node/prefer-global/buffer')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
     });
 
     it('sets `node/prefer-global/<feature>` to `always` if `preferGlobal.<feature>` is set to `true`', () => {
-      expect(
-        JSON.stringify(configResult.getRuleEntry('node', 'node/prefer-global/console')),
-      ).toMatchInlineSnapshot(`"[2,"always"]"`);
+      expect(configResult.getRuleEntry('node', 'node/prefer-global/console')).toMatchInlineSnapshot(
+        '[2, "always"]',
+      );
+    });
+
+    it('sets `node/prefer-global/console` to `never` if `preferGlobal.console` is `false`', async () => {
+      const result = await computeEslintConfig({node: {preferGlobal: {console: false}}});
+
+      expect(result.getRuleEntry('node', 'node/prefer-global/console')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
     });
 
     it('sets `node/prefer-global/<feature>` to `always` if `preferGlobal.<feature>` is not set', () => {
+      expect(configResult.getRuleEntry('node', 'node/prefer-global/process')).toMatchInlineSnapshot(
+        '[2, "always"]',
+      );
+    });
+
+    it('sets remaining `node/prefer-global/<feature>` rules to `never` when set to `false`', async () => {
+      const result = await computeEslintConfig({
+        node: {
+          preferGlobal: {
+            crypto: false,
+            process: false,
+            textDecoder: false,
+            textEncoder: false,
+            timers: false,
+            url: false,
+            urlSearchParams: false,
+          },
+        },
+      });
+
+      expect(result.getRuleEntry('node', 'node/prefer-global/crypto')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
+      expect(result.getRuleEntry('node', 'node/prefer-global/process')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
+      expect(result.getRuleEntry('node', 'node/prefer-global/text-decoder')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
+      expect(result.getRuleEntry('node', 'node/prefer-global/text-encoder')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
+      expect(result.getRuleEntry('node', 'node/prefer-global/timers')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
+      expect(result.getRuleEntry('node', 'node/prefer-global/url')).toMatchInlineSnapshot(
+        '[2, "never"]',
+      );
       expect(
-        JSON.stringify(configResult.getRuleEntry('node', 'node/prefer-global/process')),
-      ).toMatchInlineSnapshot(`"[2,"always"]"`);
+        result.getRuleEntry('node', 'node/prefer-global/url-search-params'),
+      ).toMatchInlineSnapshot('[2, "never"]');
     });
   });
 });
