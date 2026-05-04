98afe721e13be421bfd536880c6b3e64335c1e21
Andrew Kazakov <andreww_dev@proton.me>
2026-05-04 21:06:35 +0300

    chore(sonar): disable all test rules by default

---
+++ .changeset/light-breads-matter.md
@@ -0,0 +1,5 @@
+---
+'eslint-config-un': patch
+---
+
+sonar: changed the default value of `testsRules` option to `false` because some rules enabled by this option produced non-high quality reports compared to the respective testing libraries' plugins
--- src/configs/sonar.ts
+++ src/configs/sonar.ts
@@ -25,7 +25,17 @@ export interface SonarEslintConfigOptions<
 
   /**
    * Enables rules that are specific to test or assertion libraries
-   * @default true
+   *
+   * Affected rules:
+   * - [`assertions-in-tests`](https://sonarsource.github.io/rspec/#/rspec/S5850/javascript)
+   * - [`chai-determinate-assertion`](https://sonarsource.github.io/rspec/#/rspec/S6092/javascript)
+   * - [`disabled-timeout`](https://sonarsource.github.io/rspec/#/rspec/S6080/javascript)
+   * - [`inverted-assertion-arguments`](https://sonarsource.github.io/rspec/#/rspec/S3415/javascript)
+   * - [`no-code-after-done`](https://sonarsource.github.io/rspec/#/rspec/S6079/javascript)
+   * - [`no-incomplete-assertions`](https://sonarsource.github.io/rspec/#/rspec/S2970/javascript)
+   * - [`no-same-argument-assert`](https://sonarsource.github.io/rspec/#/rspec/S5863/javascript)
+   * - [`stable-tests`](https://sonarsource.github.io/rspec/#/rspec/S5973/javascript)
+   * @default false
    */
   testsRules?: boolean;
 }
@@ -39,7 +49,7 @@ export default (async (context, optionsRaw) => {
   const optionsResolved = assignDefaults(optionsRaw, {
     enableAwsRules: awsCdkLibInstalled,
     enableHelmetRules: helmetInstalled,
-    testsRules: true,
+    testsRules: false,
   } satisfies SonarEslintConfigOptions);
 
   const {enableAwsRules, enableHelmetRules, testsRules} = optionsResolved;
