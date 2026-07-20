af338485f3d9e329d870dad6adcfa70871468d51
Andrew Kazakov <andreww_dev@proton.me>
2026-07-20 22:08:50 +0300

    chore(storybook): update eslint-plugin-storybook to v10.5.3

---
+++ .changeset/blue-fans-hide.md
@@ -0,0 +1,5 @@
+---
+'eslint-config-un': patch
+---
+
+storybook: updated [`eslint-plugin-storybook` from v10.4.6 to v10.5.3](https://github.com/storybookjs/storybook/compare/v10.4.6...v10.5.3)
--- src/loaders/plugins.ts
+++ src/loaders/plugins.ts
@@ -574,8 +574,12 @@ export const pluginsLoaders = {
   storybook: genModuleLoader(
     'storybook',
     'eslint-plugin-storybook',
-    // @ts-expect-error types mismatch
-    () => interopDefault(import('eslint-plugin-storybook')) as Promise<EslintPlugin>,
+    () =>
+      // @ts-expect-error types mismatch
+      interopDefault(
+        import('eslint-plugin-storybook'),
+        // @ts-expect-error types mismatch
+      ) satisfies Promise<EslintPlugin> as Promise<EslintPlugin>,
   ),
   // We can't `import()` `@stylistic/eslint-plugin` because it's `require()`d by eslint-plugin-vue: https://github.com/vuejs/eslint-plugin-vue/blob/1b634549a9e91231e5ea79313763c69f93e678c1/lib/utils/index.js#L113 and `import()`ing after `require()`ing causes `ERR_INTERNAL_ASSERTION` error, see https://github.com/nodejs/node/issues/54577
   stylistic: genModuleLoader('stylistic', '@stylistic/eslint-plugin', () =>
