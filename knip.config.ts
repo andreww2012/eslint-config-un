08f8bb0c10122ab05e7649addff7ffa5d2c0a9bf
Andrew Kazakov <andreww_dev@proton.me>
2026-07-31 11:14:54 +0300

    chore(deps): update knip to v6.30.0

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,13 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import type {KnipConfig} from 'knip';
 
 const config: KnipConfig = {
-  ignore: ['test/**/fixtures/**'],
+  ignore: [
+    'test/**/fixtures/**',
+    // Since v6.30.0, it started to report unresolved imports in the `dist` directory for some reason
+    ...(fs.existsSync(path.join(import.meta.dirname, 'dist')) ? ['dist/**'] : []),
+  ],
   entry: [
     '.ncurc.js', // cspell:disable-line
     '.puppeteerrc.js', // cspell:disable-line
