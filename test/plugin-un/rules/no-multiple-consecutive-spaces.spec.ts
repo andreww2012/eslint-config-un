8060f468aa84a3f0556eabca815c9b4c3742834d
Andrew Kazakov <andreww_dev@proton.me>
2026-08-10 22:22:04 +0300

    test(un): add `un/no-multiple-consecutive-spaces` rule tests

---
+++ test/plugin-un/rules/no-multiple-consecutive-spaces.spec.ts
@@ -0,0 +1,135 @@
+/* eslint-disable vitest/require-hook -- `RuleTester` generates its own `describe`/`it` blocks */
+/* eslint-disable un/no-multiple-consecutive-spaces -- the test cases are made of them */
+import {RuleTester} from 'eslint';
+import {noMultipleConsecutiveSpaces} from '../../../src/plugin-un/rules/no-multiple-consecutive-spaces';
+
+const RULE_NAME = 'no-multiple-consecutive-spaces';
+
+const ruleTester = new RuleTester();
+
+ruleTester.run(RULE_NAME, noMultipleConsecutiveSpaces, {
+  valid: [
+    "const value = 'a b';",
+    "const value = '';",
+    'const value = 42;',
+    'const value = /a  b/;',
+    'const value = `a  b`;',
+
+    "const value = '  ';",
+    {
+      code: "const value = '  ';",
+      options: [{allowSpacesOnly: true}],
+    },
+  ],
+  invalid: [
+    {
+      code: "const value = 'a  b';",
+      errors: [
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "const value = 'a b';",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "const value = 'a   b';",
+      errors: [
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "const value = 'a b';",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "const value = 'a  b  c';",
+      errors: [
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "const value = 'a b  c';",
+            },
+          ],
+        },
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "const value = 'a  b c';",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "const value = {'a  b': 1};",
+      errors: [
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "const value = {'a b': 1};",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "'a  b';",
+      errors: [
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "'a b';",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "const value = '  ';",
+      options: [{allowSpacesOnly: false}],
+      errors: [
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "const value = ' ';",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: "const value = ' a  ';",
+      options: [{allowSpacesOnly: true}],
+      errors: [
+        {
+          messageId: 'noMultipleConsecutiveSpaces',
+          suggestions: [
+            {
+              messageId: 'replaceMultipleSpacesWithSingle',
+              output: "const value = ' a ';",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
