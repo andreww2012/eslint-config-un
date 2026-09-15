0bbbce18240821c5b47ae016a4bb2b0749833aba
Andrew Kazakov <andreww_dev@proton.me>
2026-09-15 15:51:24 +0300

    chore(jsdoc): update eslint-plugin-jsdoc to v64.5.0

--- .changeset/twenty-days-add.md
+++ .changeset/twenty-days-add.md
@@ -5,3 +5,4 @@
 jsdoc: updated [`eslint-plugin-jsdoc` from v63.3.3 to v64.4.0](https://github.com/gajus/eslint-plugin-jsdoc/compare/v63.3.3...v64.4.0):
 
 - 🟢 enabled [`jsdoc/no-unnecessary-type-assertion`](https://github.com/gajus/eslint-plugin-jsdoc/blob/HEAD/docs/rules/no-unnecessary-type-assertion.md) rule
+- 🔴 not enabled [`jsdoc/ts-ban-ts-comment`](https://github.com/gajus/eslint-plugin-jsdoc/blob/HEAD/docs/rules/ts-ban-ts-comment.md) rule
--- src/configs/jsdoc.ts
+++ src/configs/jsdoc.ts
@@ -472,6 +472,7 @@ export default defineUnConfig<JsdocEslintConfigOptions>(
     .addRule('sort-tags', ERROR) /** @since 37.8.0 */
     .addRule('tag-lines', ERROR) /** @since 34.4.0 */ // 🟢4️⃣
     .addRule('text-escaping', OFF) /** @since 39.5.0 */ // 1️⃣
+    .addRule('ts-ban-ts-comment', OFF) /** @since 64.5.0 */
     .addRule('ts-method-signature-style', ERROR, ['property' /* Default */]) /** @since 61.1.0 */
     .addRule('ts-no-empty-object-type', ERROR) /** @since 61.1.0 */ // 🟢
     .addRule('ts-no-unnecessary-template-expression', ERROR) /** @since 61.1.0 */
